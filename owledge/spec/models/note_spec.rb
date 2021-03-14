fa539c048b54de5031eda87e293d12d3e7f9544e
George Cherian <georgcher@gmail.com>
2021-03-14 14:58:24 -0400

    Created more tests for Note Model

--- owledge/spec/models/note_spec.rb
+++ owledge/spec/models/note_spec.rb
@@ -178,4 +178,70 @@ RSpec.describe Note, type: :model do
       end
     end
   end
+
+  
+  describe 'check if the child function is working' do
+    context 'IF records exist in the table then check the children for them' do
+      before :each do
+        Note.delete_all
+        @note = Note.create([{
+                              title: 'Graph Theory',
+                              description: %w{
+                                In mathematics, graph theory is the study of graphs, which are mathematical structures
+                                used to model pairwise relations between objects.
+                              }.join(" ")
+                            }])
+        @note2 = Note.create([{
+                              title: 'Sorting Algorithms',
+                              description: %w{
+                                In computer science, a sorting algorithm is an algorithm that puts elements of a list in a certain order.
+                              }.join(" ")
+                            }])
+        Note.create([{
+                              title: 'Sorting Algorithms 3',
+                              description: %w{
+                                In computer science, a sorting algorithm is an algorithm that puts elements of a list in a certain order.
+                              }.join(" "),
+                              parent_id:@note[0].id
+                            }])
+        Note.create([{
+                              title: 'Sorting Algorithms 4',
+                              description: %w{
+                                In computer science, a sorting algorithm is an algorithm that puts elements of a list in a certain order.
+                              }.join(" "),
+                              parent_id:@note[0].id
+                            }])
+        @child_note = Note.create([{
+                              title: 'Sorting Algorithms 5',
+                              description: %w{
+                                In computer science, a sorting algorithm is an algorithm that puts elements of a list in a certain order.
+                              }.join(" "),
+                              parent_id:@note[0].id
+                            }])
+        Note.create([{
+                              title: 'Sorting Algorithms 6',
+                              description: %w{
+                                In computer science, a sorting algorithm is an algorithm that puts elements of a list in a certain order.
+                              }.join(" "),
+                              parent_id:@child_note[0].id
+                            }])
+      end
+      it 'Check children of parent notebook' do
+        @note = Note.find(1)
+        @note_children = @note.children
+        expect(@note_children.count).to eq(3)
+
+      end
+      it 'Check children of a parent' do
+        @note = Note.find(2)
+        @note_children = @note.children
+        expect(@note_children.count).to eq(0)
+      end
+      it 'Check children of another child' do
+        @note = Note.find(5)
+        @note_children = @note.children
+        expect(@note_children.count).to eq(1)
+      end
+    end
+  end
 end
