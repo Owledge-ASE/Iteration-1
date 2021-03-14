b5807a6167f614d08aa3391f17127798dc4950ef
George Cherian <georgcher@gmail.com>
2021-03-14 11:57:48 -0400

    minor UI and cucumber changes

--- owledge/db/seeds.rb
+++ owledge/db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-graph_theory = Note.create({
+graph_theory = Note.create([{
                               title: 'Graph Theory',
                               description: %w{
                                 In mathematics, graph theory is the study of graphs, which are mathematical structures
