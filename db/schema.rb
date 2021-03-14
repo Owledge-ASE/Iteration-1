2f84aaf496cfdb6dae6d0292f618251e8a87ea88
George Cherian <georgcher@gmail.com>
2021-03-14 13:12:04 -0400

    Added test for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,48 @@
 
 ActiveRecord::Schema.define(version: 2021_03_14_042129) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "notebook_to_sub_notes", force: :cascade do |t|
+    t.string "notebook_id"
+    t.string "subnote_id"
+    t.datetime "created_timestamp"
+    t.datetime "update_timestamp"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notebooks", force: :cascade do |t|
+    t.string "notebook_id"
+    t.text "description"
+    t.string "title"
+    t.datetime "created_timestamp"
+    t.datetime "update_timestamp"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.integer "parent_id"
+    t.bigint "parent_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["parent_id"], name: "index_notes_on_parent_id"
+  end
+
+  create_table "sub_notes", force: :cascade do |t|
+    t.string "subnote_id"
+    t.text "description"
+    t.string "title"
+    t.datetime "created_timestamp"
+    t.datetime "update_timestamp"
+    t.string "parent_id"
+    t.string "like"
+    t.string "dislikes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "notes", "notes", column: "parent_id"
