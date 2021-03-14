a1591d96a42f8475011ac0d56dea0070bbd54b0b
Sean Capaloff Jones <smcjones@gmail.com>
2021-03-14 00:31:38 -0500

    initial view for show

--- owledge/app/controllers/notebooks_controller.rb
+++ owledge/app/controllers/notebooks_controller.rb
@@ -1,16 +1,12 @@
-
-
-
-
-
 class NotebooksController < ApplicationController
   def new
 
   end
   def show
-    
+    id = params[:id]
+    @notes = Note.find(id)
   end
   def index
-    @notes = SubNote.allParents
+    @notes = Note.allParents
   end
 end
