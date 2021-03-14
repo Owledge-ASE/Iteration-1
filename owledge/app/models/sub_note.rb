23ee718844445a94ace68b3e7a3f0cea703059e5
George Cherian <georgcher@gmail.com>
2021-03-13 22:46:46 -0500

    Basic index.html and controller code

--- owledge/app/models/sub_note.rb
+++ owledge/app/models/sub_note.rb
@@ -1,2 +1,5 @@
 class SubNote < ApplicationRecord
+    def self.allParents
+        return self.where('parent_id is null or parent_id=""')
+    end
 end
--- owledge/db/seeds.rb
+++ owledge/db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-notebook = Notebook.create([{notebook_id:"1", title: 'Star Wars' }, {notebook_id:"2", title: 'Lord of the Rings' }])
+Notebook.create([{notebook_id:"1", title: 'Star Wars' }, {notebook_id:"2", title: 'Lord of the Rings' }])
+SubNote.create([{subnote_id:"1", title: 'Star Wars' }, {subnote_id:"2", title: 'Lord of the Rings',parent_id:"1" }])
+SubNote.create([{subnote_id:"3", title: 'Graph Theory',description:"sddddddddddddddddddddddddddddddddddddddddddddddddddddd" }])
