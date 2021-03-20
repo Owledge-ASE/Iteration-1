f8d128415b933e8c05c91ccd3401a1cb919419fb
Sean Capaloff Jones <smcjones@gmail.com>
2021-03-20 02:12:24 -0400

    added user login paths

--- owledge/config/routes.rb
+++ owledge/config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  resources :notebooks 
+  devise_for :users, path: '', skip: [:sessions, :registrations]
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    get 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'register', to: 'devise/registrations#new', as: :new_user_registration
+    post 'register', to: 'devise/registrations#create', as: :user_registration
+    get 'profile', to: 'devise/registrations#edit', as: :edit_user_registration
+    delete 'register', to: 'devise/registrations#destroy', as: :destroy_user
+  end
 
-  root :to => redirect('/notebooks')
+  resources :notebooks
 
-  get 'users/new' => 'users#new' as :new_user
-  post 'users' => 'users#create'
+  root to: "notebooks#index"
 
+  get 'login', to: 'devise/sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
