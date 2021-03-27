af88f56408e5a3805aa1b6a4ec78968815279062
Sean Capaloff Jones <smcjones@gmail.com>
2021-03-27 18:33:58 -0400

    added more coverage

--- owledge/app/models/user.rb
+++ owledge/app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :user_comments, :foreign_key => :user_id
 
   def display_name
-    first_name + " " + last_name
+    if first_name.nil?
+      last_name
+    else
+      first_name + " " + last_name
+    end
   end
 end
